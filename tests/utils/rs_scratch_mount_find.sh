8dc7cce7b91280ad0cd46b15e4c887c0498be3df
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-02-08 19:27:18 +0000

    Removed extra user name from path
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@861 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/utils/rs_scratch_mount_find.sh
+++ tests/utils/rs_scratch_mount_find.sh
@@ -9,7 +9,8 @@ if [ "$scratch_mount" == "" ]; then
    scratch_mount="None"
 else 
    if [ ! -e $scratch_mount/$USER ]; then
-       scratch_mount="`find /panfs -maxdepth 3 -name $USER | head -1`"
+       scratch_dir="`find /panfs -maxdepth 3 -name $USER | head -1`"
+       scratch_mount="`dirname $scratch_dir`" 
    fi
 fi
 
