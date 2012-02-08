bd2f63bbdcd25cf93d09b0138680263d18196267
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-02-08 18:32:42 +0000

    Fixed to handle non sym link panfs mount points
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@859 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/utils/rs_scratch_mount_find.sh
+++ tests/utils/rs_scratch_mount_find.sh
@@ -7,5 +7,10 @@ scratch_mount="`mount -t panfs | awk '{print $3}' | sed 's/\// /g' | awk '{print
 if [ "$scratch_mount" == "" ]; then 
    echo "Error:  Unable to find panfs scratch space."
    scratch_mount="None"
+else 
+   if [ ! -e $scratch_mount/$USER ]; then
+       scratch_mount="`find /panfs -maxdepth 3 -name atorrez | head -1`"
+   fi
 fi
+
 echo $scratch_mount
