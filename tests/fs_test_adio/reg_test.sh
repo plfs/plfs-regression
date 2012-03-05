bd3f1f0929035dd8037fa93a63f0d6ebfb1a27f6
dshrader <dshrader@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-03-05 15:44:15 +0000

    As these scripts are generated in the regression tests, they shouldn't be checked in.
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@934 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/fs_test_adio/reg_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /users/atorrez/iotests/regression//tests/utils/rs_env_init.sh
-echo PATH=$PATH
-echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-mpirun -n 16 /users/atorrez/iotests/regression/inst/test_fs/fs_test.rrz.x -target plfs:/var/tmp/plfs.atorrez/atorrez/rrz.adio_write_read.out -strided 0 -nobj 4 -nodb -shift -type 2 -deletefile -size 1048760
-
-# 1 jobs dispatched by list.
