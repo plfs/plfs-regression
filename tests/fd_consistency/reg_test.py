be7129182a3865fb130ae936b9e6839bba82593e
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-03-30 16:30:01 +0000

    Updated test to handle shared_file (n-1) and file_per_proc(n-n) mountpoints.
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@1003 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/fd_consistency/reg_test.py
+++ tests/fd_consistency/reg_test.py
@@ -29,65 +29,19 @@ def main(argv=None):
     # generated script
     input_script = "input_test.py"
     # Walltime of the job(s)
-    walltime = "60:00"
-
-    # Get the mount_point. No need to check because if there was a problem,
-    # it would have been found in getting the target.
-    mnt_pt = tc.get_mountpoint()
-    
-    # prescript and postscript
-    prescript = (tc.basedir + "tests/utils/rs_plfs_fuse_mount.sh "
-        + str(mnt_pt) + " serial")
-    postscript = (tc.basedir + "tests/utils/rs_plfs_fuse_umount.sh "
-        + str(mnt_pt) + " serial")
-    find_mnt_pts = (tc.basedir + "tests/utils/rs_plfs_config_query.py -m")
+    walltime = "120:00"
 
     # Create the script
     try:
         f = open(gen_script, 'w')
         # Write the header
         f.write('#!/bin/bash\n')
-        # Write a check about rs_exprmgmtrc_target_path_append.py
-        f.write('if [ ! -x "' + str(tc.basedir)
-            + 'tests/utils/rs_exprmgmtrc_target_path_append.py" ]; then\n')
-        f.write('  echo "Failure: ' + str(tc.basedir)
-            + 'tests/utils/rs_exprmgmtrc_target_path_append.py '
-            + 'is not executable and must be"\n')
-        f.write('  exit 1\n')
-        f.write('fi\n')
-        # Write a command that will get the proper environment
-        f.write('source ' + str(tc.basedir) + 'tests/utils/rs_env_init.sh\n')
-        f.write("mount_points=" + "" + str(mnt_pt) + "" + "\n")
-        f.write("base_dir=" +"" +str(tc.basedir) + "" + "\n")
 
-        # Write into the script the script that will mount plfs
-        f.write("echo \"Running " + str(prescript) + "\"\n")
-        # The next section of code is to determine if the script needs to
-        # run the unmount command. If rs_plfs_fuse_mount.sh returns with a 1,
-        # this test is not going to issue the unmount command.
-        f.write("need_to_umount=\"True\"\n")
-        f.write(str(prescript) + "\n")
-        f.write("ret=$?\n")
-        f.write("if [  \"$ret\" == 0 ]; then\n")
-        f.write("    echo \"Mounting successful\"\n")
-        f.write("    need_to_umount=\"True\"\n")
-        f.write("elif [ \"$ret\" == 1 ]; then\n")
-        f.write("    echo \"Mount points already mounted.\"\n")
-        f.write("    need_to_umount=\"False\"\n")
-        f.write("else\n")
-        f.write("    echo \"Something wrong with mounting.\"\n")
-        f.write("    exit 1\n")
-        f.write("fi\n")
+        f.write("base_dir=" +"" +str(tc.basedir) + "" + "\n")
         f.write("plfs_tarball_path=" + str(os.getcwd()) + "\n")
         f.close()
         os.system(str("cat ") + in_script + " >> " + str(gen_script))
 
-        f = open(gen_script, 'a')
-        f.write("if [ \"$need_to_umount\" == \"True\" ]; then\n")
-        f.write("    echo \"Running " + str(postscript) + "\"\n")
-        f.write("    " + str(postscript) + "\n")
-        f.write("fi\n")
-        f.close()
         # Make the script executable
         os.chmod(gen_script, 0764)
     except (IOError, OSError), detail:
