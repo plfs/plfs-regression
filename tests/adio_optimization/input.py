0c5fc91accb27a0ddf787e20cd4f569cdfa64ab9
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-05-11 19:25:41 +0000

    Added multiple mount test run capability
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@1036 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/adio_optimization/input.py
+++ tests/adio_optimization/input.py
@@ -1,47 +1,17 @@
 #!/usr/bin/env python
 #
 # A simple read and write using adio
-
-import os,sys,re,getpass
-curr_dir = os.getcwd()
-basedir = re.sub('tests/adio_optimization.*', '', curr_dir)
-
-# Add the directory that contains helper modules
-utils_dir = basedir + "tests/utils"
-if utils_dir not in sys.path:
-    sys.path += [ utils_dir ]
-
-# Get experiment_management paths added to sys.path if needed
-import rs_exprmgmt_paths_add as emp
-emp.add_exprmgmt_paths(basedir)
-
-# Import fs_test
+import os,imp
+(fp, path, desc) = imp.find_module('test_common', [os.getcwd()])
+test_common = imp.load_module('test_common', fp, path, desc)
+fp.close()
 import fs_test
-import expr_mgmt
-
-# Import the module with functions for finding mount points and targets
-import rs_plfs_config_query
-import rs_exprmgmtrc_target_path_append as tpa
-
-# Get a target to use for this test.
-file = os.getenv("MY_MPI_HOST") + ".adio_hints.out"
-user = getpass.getuser()
-mount_points = rs_plfs_config_query.get_mountpoints()
-mount_point = mount_points[-1]
-# Get the optional rs_mnt_append_path. With passing only a single entry,
-# we'll get back a list of only one element.
-top_dir = tpa.append_path([mount_point])[0]
-target = str(top_dir) + "/" + str(file)
-
-# Want enough processes to fill up two nodes
-ppn = expr_mgmt.config_option_value("ppn")
-np = 2 * int(ppn)
 
 mpi_options = {
-    "n"     : [ np ]
+    "n"     : [ test_common.nprocs ]
 }
 
-mpi_program = ( str(basedir) + "inst/test_fs/fs_test."            
+mpi_program = ( str(test_common.basedir) + "inst/test_fs/fs_test."            
             + os.getenv("MY_MPI_HOST") + ".x" )
 
 program_options = {
@@ -52,7 +22,7 @@ program_options = {
   "type"     : [ 2 ],
   "nobj"     : [ 4 ],
   "strided"  : [ 0 ],
-  "target"   : [ "plfs:" + str(target) ],
+  "target"   : [ "plfs:$path" ],
   "hints"    : [ "plfs_disable_broadcast=1,plfs_disable_paropen=1" ]
 }
 
