eb2d04a7fdd03705796f5d0bee6891442e93213b
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-05-11 19:27:55 +0000

    Added multiple mount test run capability
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@1039 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/adio_nto1_strided/reg_test.py
+++ tests/adio_nto1_strided/reg_test.py
@@ -2,7 +2,7 @@
 #
 # Run a simple write/read test using fuse.
 
-import os,sys,re,imp
+import os,sys,re,imp,subprocess
 
 # Load the common.py module to get common variables
 (fp, path, desc) = imp.find_module('test_common', [os.getcwd()])
@@ -28,23 +28,13 @@ def main(argv=None):
     # generated script
     input_script = "input_test.py"
     # Walltime of the job(s)
-    walltime = "10:00"
-
-    # Figure out the target for this test
-    target = tc.get_target()
-    if target == None:
-        print ("Error getting target")
+    walltime = "20:00"
+    
+    # get all mountpoints and associated target path and file
+    target_paths = tc.get_target_paths()
+    if target_paths == None:
+        print ("Error getting mountpoint")
         return [-1]
-    
-    # Get the mount_point. No need to check because if there was a problem,
-    # it would have been found in getting the target.
-    mnt_pt = tc.get_mountpoint()
-    
-    # prescript and postscript
-    prescript = (tc.basedir + "/tests/utils/rs_plfs_fuse_mount.sh "
-        + str(mnt_pt))
-    postscript = (tc.basedir + "/tests/utils/rs_plfs_fuse_umount.sh "
-        + str(mnt_pt))
 
     # Create the script
     try:
@@ -53,12 +43,20 @@ def main(argv=None):
         f.write('#!/bin/bash\n')
         # Write a command that will get the proper environment
         f.write('source ' + str(tc.basedir) + '/tests/utils/rs_env_init.sh\n')
+        # Convert target path list into space delimited targets 
+        paths=' '.join(target_paths)
+        # Create a for loop to iterate throuh all mountpoints/paths
+        f.write('for path in ' + str(paths) + '\n')
+        f.write('do\n')
+        f.write('   echo Using $path as target\n')
+        fs_test_command = str(tc.em_p.get_expr_mgmt_dir(tc.basedir)) + "/run_expr.py --dispatch=list " + str(input)
+
+        cmd = subprocess.Popen([fs_test_command], stdout=subprocess.PIPE, shell=True)
+        fs_test_run, errors = cmd.communicate()
+        f.write("   " + str(fs_test_run) + '\n')
+        f.write('done\n')
+
         f.close()
-        # Generate the fs_test command through experiment_management
-        os.system(str(tc.em_p.get_expr_mgmt_dir(tc.basedir))
-            + "/run_expr.py --dispatch=list " + str(input) + " >> " + str(gen_script))
-        # Make the script executable
-        os.chmod(gen_script, 0764)
     except (IOError, OSError), detail:
         print ("Problem with creating script " + str(gen_script) + ": "
             + str(detail))
