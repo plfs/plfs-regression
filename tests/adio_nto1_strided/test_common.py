c9295eb8fd950ca84521ad2a6412ce4253a0bdbf
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-05-11 19:27:30 +0000

    Added multiple mount test run capability
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@1037 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/adio_nto1_strided/test_common.py
+++ tests/adio_nto1_strided/test_common.py
@@ -27,26 +27,26 @@ em_p.add_exprmgmt_paths(basedir)
 # enough to cover at least two nodes.
 import expr_mgmt
 ppn = expr_mgmt.config_option_value("ppn")
-nprocs = 2 * int(ppn)
+nprocs = 4 * int(ppn)
 
 # Import the module with functions for finding mount points.
 import rs_plfs_config_query
 import rs_exprmgmtrc_target_path_append as tpa
 
-def get_mountpoint():
+def get_target_paths():
+    mount_path = []
     mount_points = rs_plfs_config_query.get_mountpoints()
     if len(mount_points) > 0:
-        mount_point = mount_points[-1]
+        for mnt in mount_points:
+            top_dir = tpa.append_path([mnt])[0]
+            target = str(top_dir) + "/" + str(file)
+            mount_path.append(target)
     else:
-        mount_point = None
-    return mount_point
-    
-def get_target():
-    mount_point = get_mountpoint()
-    if mount_point != None:
-        top_dir = tpa.append_path([mount_point])[0]
-        target = str(top_dir) + "/" + str(file)
-    else:
-        target = None
-    return target
+        mount_path = None
+    return mount_path
+
+# Returns the number of mount points found
+def get_mountpoint_cnt():
+    mount_points = rs_plfs_config_query.get_mountpoints()
+    return len(mount_points)
 
