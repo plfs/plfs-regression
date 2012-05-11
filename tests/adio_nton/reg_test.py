4c08cd8f6da31a48a7a77220013287080733e7e2
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-05-11 19:35:55 +0000

    Added multiple mount test run capability
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@1041 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/adio_nton/reg_test.py
+++ tests/adio_nton/reg_test.py
@@ -2,25 +2,14 @@
 #
 # Run a plfs regression test.
 
-import os,sys,re
+import os,sys,re,imp,subprocess
 
-# Figure out the base directory of the regression suite
-curr_dir = os.getcwd()
-basedir = re.sub('tests/adio_nton.*', '', curr_dir)
-
-# Add the directory that contains the helper scripts
-utils_dir = basedir + "tests/utils"
-if utils_dir not in sys.path:
-    sys.path += [ utils_dir ]
-
-import rs_plfs_config_query
-
-# Add the experiment_management locations to sys.path
-import rs_exprmgmt_paths_add as emp
-emp.add_exprmgmt_paths(basedir)
+# Load the common.py module to get common variables
+(fp, path, desc) = imp.find_module('test_common', [os.getcwd()])
+tc = imp.load_module('test_common', fp, path, desc)
+fp.close()
 
 import run_expr
-import expr_mgmt
 
 def main(argv=None):
     """The main routine for submitting and running a test.
@@ -30,25 +19,19 @@ def main(argv=None):
     if argv == None:
         argv = sys.argv
 
+    # Experiment_management script to run
+    input = tc.curr_dir + "/input.py"
     # Generated script
     script = "reg_test.sh"
     # Script that can be passed to experiment_management that will run the
     # generated script
     input_script = "input_script.py"
-    walltime = "15:00"
-    # Make sure this matches what is in input.py for n in mpi_options. Want
-    # enough to cover at least two nodes.
-    ppn = expr_mgmt.config_option_value("ppn")
-    nprocs = 2 * int(ppn)
+    walltime = "25:00"
 
-    # Make sure we can get a valid mount_point. The variable mount_points is
-    # not used again, but this code is valuable because where we do use it
-    # in one of the other scripts, there is no error checking to make sure we
-    # got a valid mount point and target. So the following is just for
-    # error checking.
-    mount_points = rs_plfs_config_query.get_mountpoints()
-    if len(mount_points) == 0:
-        print ("Error getting a valid mount point.")
+    # get all mountpoints and associated target path and file
+    target_paths = tc.get_target_paths()
+    if target_paths == None:
+        print ("Error getting mountpoint")
         return [-1]
 
     # Create the script
@@ -56,24 +39,33 @@ def main(argv=None):
         f = open(script, 'w')
         f.write('#!/bin/bash\n')
         # Set up the environment
-        f.write('source ' + str(basedir) + '/tests/utils/rs_env_init.sh\n')
+        f.write('source ' + str(tc.basedir) + '/tests/utils/rs_env_init.sh\n')
         # Check environment variables to make sure we're using the right
         # binaries and libraries.
         f.write("echo PATH=$PATH\n")
         f.write("echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH\n")
+
+        # Convert target path list into space delimited targets 
+        paths=' '.join(target_paths)
+        # Create a for loop to iterate throuh all mountpoints/paths
+        f.write('for path in ' + str(paths) + '\n')
+        f.write('do\n')
+        f.write('   echo Using $path as target\n')
+        fs_test_command = str(tc.em_p.get_expr_mgmt_dir(tc.basedir)) + "/run_expr.py --dispatch=list " + str(input)
+
+        cmd = subprocess.Popen([fs_test_command], stdout=subprocess.PIPE, shell=True)
+        fs_test_run, errors = cmd.communicate()
+        f.write("   " + str(fs_test_run) + '\n')
+        f.write('done\n')
         f.close()
-        os.system(str(basedir) + "/inst/experiment_management/run_expr.py "
-            + "--dispatch=list "
-            + "./input.py >> " + str(script))
-        os.chmod(script, 0764)
     except (IOError, OSError), detail:
         print ("Problem with creating script " + str(script) 
             + ": " + str(detail))
         return [-1]
 
     # Run the script
-    last_id = run_expr.main(['run_expr', str(curr_dir) + "/" + str(input_script),
-        '--nprocs=' + str(nprocs), '--walltime=' + str(walltime), 
+    last_id = run_expr.main(['run_expr', str(tc.curr_dir) + "/" + str(input_script),
+        '--nprocs=' + str(tc.nprocs), '--walltime=' + str(walltime), 
         '--dispatch=msub'])
     return [last_id]
 
