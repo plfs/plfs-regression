7b7e9b119b9f296a50ed30fd07f8684511ab8c41
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-05-29 21:16:21 +0000

    Added multiple mount test run capability
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@1060 979fb63a-54b4-4514-8d67-9ab8ea1b59df

--- tests/posix_nton/test_common.py
+++ tests/posix_nton/test_common.py
@@ -31,22 +31,14 @@ nprocs = 2 * int(ppn)
 
 # Import the module with functions for finding mount points.
 import rs_plfs_config_query
-import rs_exprmgmtrc_target_path_append as tpa
 
-def get_mountpoint():
+# Return the filename defined here
+def get_filename():
+    return file
+
+# Return a list of mount_points
+def get_mountpoints():
     mount_points = rs_plfs_config_query.get_mountpoints()
-    if len(mount_points) > 0:
-        mount_point = mount_points[-1]
-    else:
-        mount_point = None
-    return mount_point
-    
-def get_target():
-    mount_point = get_mountpoint()
-    if mount_point != None:
-        top_dir = tpa.append_path([mount_point])[0]
-        target = str(top_dir) + "/" + str(file)
-    else:
-        target = None
-    return target
-
+    if len(mount_points) <= 0:
+        mount_points = None
+    return mount_points
