2ad0ca32637e82affac743aab708f6a003033586
atorrez3 <atorrez3@979fb63a-54b4-4514-8d67-9ab8ea1b59df>
2012-06-18 20:05:09 +0000

    New test created
    git-svn-id: https://plfs.svn.sourceforge.net/svnroot/plfs/regression@1125 979fb63a-54b4-4514-8d67-9ab8ea1b59df

---
+++ tests/adio_nto1_overwrite/input.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# A test where fs_test writes a file, a single character in the file is
+# changed, and then fs_test reads it in. There should be exactly one
+# error reported.
+
+import imp,os
+(fp, path, desc) = imp.find_module('test_common', [os.getcwd()])
+test_common = imp.load_module('test_common', fp, path, desc)
+fp.close()
+
+import fs_test
+
+mpi_options = {
+    "n"     : [ test_common.nprocs ]
+}
+
+mpi_program = ( str(test_common.basedir) + "inst/test_fs/fs_test."            
+            + os.getenv("MY_MPI_HOST") + ".x" )
+
+program_options = {
+  "size"       : [ '$size' ],
+  "shift"      : [ '' ],
+  "nodb"       : [ ''],
+  "type"       : [ 2 ],
+  "nobj"       : [ 1 ],
+  "strided"    : [ 0 ],
+  "io"         : [ 'mpi' ],
+  "touch"      : [ '3' ],
+  "check"      : [ '3' ],
+  "target"     : [ 'plfs:$path' ]
+}
+
+# fs_test doesn't need program_arguments
+
+def get_commands(expr_mgmt_options):
+  global mpi_options, mpi_program, program_options 
+  return fs_test.get_commands( mpi_options=mpi_options, 
+          mpi_program=mpi_program, program_options=program_options,
+          n1_strided=True, n1_segmented=False, nn=False,
+          expr_mgmt_options=expr_mgmt_options )
