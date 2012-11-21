3aafa4a11131cc88c68f024335e1d3b8c487cff6
David L. Shrader <dshrader@rrz-fe1.localdomain>
2012-11-21 12:34:26 -0700

    Remove old mount point checking in fd_consistency's fd_test.sh

--- tests/fd_consistency/fd_test.sh
+++ tests/fd_consistency/fd_test.sh
@@ -10,7 +10,6 @@ fd_prev_cnt=4
 #
 cnt_max=10
 #
-build_for_mnt=1
 user=${USER}
 
 #plfs_tarball_path=`pwd`
@@ -54,21 +53,14 @@ else
               continue
            fi
 
-           the_mp=`df | grep ${mnt} | awk '{print $6}'`
-           echo $the_mp
-           if [ $the_mp != $mnt ]; then
-             build_for_mnt=1
-           fi 
            target=`$base_dir/tests/utils/rs_exprmgmtrc_target_path_append.py $mnt`
 
            # loop cnt times building of plfs
            cnt=0
            while [ $cnt -lt $cnt_max ]
            do
-             let "cnt += 1"
-             echo "Test iteration $cnt on mount $mnt" 
-             if [ $build_for_mnt -eq 1 ]; then
-
+               let "cnt += 1"
+               echo "Test iteration $cnt on mount $mnt" 
                #setup to build plfs from tarball      
                echo "Copying $plfs_tarball_path/$plfs_tarball to $target"
                cp $plfs_tarball_path/$plfs_tarball $target/.
@@ -142,7 +134,6 @@ else
                echo ""
                echo "Completed interation $cnt of $cnt_max on $mnt"
                echo ""
-             fi
            done # end while loop
            echo "Removing plfs tarball and directory from $target"
 
